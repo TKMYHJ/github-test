57e8bb982071d341ecfbfecdf89bf39ee57d1fdd
ming <ming@example.com>
2017-09-26 10:18:51 +0800

    添加了一个文件夹

--- leijicheng/tabtenn0.cpp
+++ leijicheng/tabtenn0.cpp
@@ -8,4 +8,18 @@ TableTennisPlayer::TableTennisPlayer (const string &fn,
 
 std::cout<<lastname<<","<<firstname;
 
+
 }
+
+RatedPlayer::RatedPlayer(unsigned int r,const string &fn,
+			const string &ln, bool ht):TableTennisPlayer(fn,ln,ht)
+{
+ rating =r;
+
+}
+
+RatedPlayer::RatedPlayer(unsigned int r, const TableTennisPlayer &tp)
+			:TableTennisPlayer(tp),rating (r)
+{
+}
+
--- leijicheng/tabtenn0.h
+++ leijicheng/tabtenn0.h
@@ -17,4 +17,15 @@ void ResetTable(bool v) { hasTable=v;};
 
 };
 
+class RatedPlayer:public TableTennisPlayer
+{
+private:
+	unsigned int rating;
+public:
+	RatedPlayer (unsigned int r=0, const string &fn="none",
+			const string & ln="none",bool ht=false);
+	RatedPlayer (unsigned int r, const TableTennisPlayer &tp);
+	unsigned int ResetRating() const {return rating;}
+	void ResetRating (unsigned int r) {rating =r;}
+}
 #endif
